bbcddcb4138675a9243137a131b1b77ad101e455
Marco <marco.salman@gmail.com>
2022-07-03 22:54:28 -0400

    exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+Store.has_many :employees
+
+Employee.belongs_to :store
+
+@store1.employees.create(first_name: "Marco", last_name: "Gamal", hourly_rate: 120)
+@store1.employees.create(first_name: "Asta", last_name: "Gamal", hourly_rate: 110)
+@store1.employees.create(first_name: "Kioko", last_name: "Gamal", hourly_rate: 65)
+
+@store2.employees.create(first_name: "Sora", last_name: "Gamal", hourly_rate: 50)
+@store2.employees.create(first_name: "Gamal", last_name: "Abdul", hourly_rate: 160)
+@store2.employees.create(first_name: "Widia", last_name: "Gamal", hourly_rate: 70)
